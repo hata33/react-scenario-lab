6eda86fc9640bb41cd09be5b703158ebbfe40747
hata <1553126902@qq.com>
2025-11-28 16:34:32 +0800

    feat(types): 添加 bpmn-js 多语言类型声明
    为 bpmn-js 的国际化翻译文件添加 TypeScript 类型声明，
    包括中文、英文、德文、法文、意大利文、日文、波兰文、
    葡萄牙文、俄文和西班牙文等语言模块的类型定义。

---
+++ src/components/bpmn/translations/types.d.ts
@@ -0,0 +1,70 @@
+// 类型声明文件
+declare module 'bpmn-js-i18n/translations/zn' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/en' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/de' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/fr' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/it' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/ja' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/pl' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/pt' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/ru' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
+
+declare module 'bpmn-js-i18n/translations/es' {
+	const translations: {
+		[key: string]: string;
+	};
+	export default translations;
+}
